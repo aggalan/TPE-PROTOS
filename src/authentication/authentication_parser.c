57042de5b2ce30ecd424f1b6fe68982bbacb51d8
ivovila <ivilamowski@itba.edu.ar>
2025-06-27 15:34:00 -0300

    logs

--- src/authentication/authentication_parser.c
+++ src/authentication/authentication_parser.c
@@ -15,6 +15,7 @@ verify_credentials(const char *user, const char *pass) {
     char fpass[P_MAX_LEN + 1];
 
     while (fscanf(f, " %15s %15s", fuser, fpass) == 2) {
+        printf("DBG: leído user='%s' pass='%s'\n", fuser, fpass);
         if (strcmp(user, fuser) == 0 && strcmp(pass, fpass) == 0) {
             fclose(f);
             return true;
@@ -43,6 +44,7 @@ authentication_parse(AuthParser *p, buffer *b) {
         switch (p->state) {
 
             case AUTH_VERSION:
+                printf("AUTH_VERSION: %d\n", c);
                 if (c == VERSION) {
                     p->version = c;
                     p->state   = ULEN;
@@ -52,6 +54,7 @@ authentication_parse(AuthParser *p, buffer *b) {
                 break;
 
             case ULEN:
+                printf("ULEN: %d\n", c);
                 if (c == 0 || c > U_MAX_LEN) {
                     p->state = AUTH_ERROR;
                     break;
@@ -71,6 +74,8 @@ authentication_parse(AuthParser *p, buffer *b) {
                 break;
 
             case PLEN:
+                printf("name: %s\n", p->uname);
+                printf("PLEN: %d\n", c);
                 if (c == 0 || c > P_MAX_LEN) {
                     p->state = AUTH_ERROR;
                     break;
@@ -84,15 +89,20 @@ authentication_parse(AuthParser *p, buffer *b) {
                 p->passwd[p->idx++] = (char)c;
                 if (p->idx == p->plen) {
                     p->passwd[p->idx] = '\0';
+
+                    printf("passwd: %s\n", p->passwd);
                     p->auth_check = verify_credentials(p->uname, p->passwd)
                                     ? AUTH_SUCCESS : AUTH_FAILURE;
+                    printf("AUTH_CHECK: %d\n", p->auth_check);
                     p->state = AUTH_END;
                     return p->state;
                 }
                 break;
 
             case AUTH_END:
+                printf("AUTH_END: %d\n", c);
             case AUTH_ERROR:
+                printf("AUTH_ERROR: %d\n", c);
         }
     }
     return p->state;
@@ -112,6 +122,7 @@ AuthCodes
 fill_authentication_answer(AuthParser *p, buffer *b) {
     if (!buffer_can_write(b))
         return AUTH_REPLY_FULL_BUFFER;
+    printf("Filling Auth answer... \n");
     buffer_write(b, VERSION);
     buffer_write(b, (p->auth_check == AUTH_SUCCESS) ? 0x00 : 0x01);
     return p->auth_check == AUTH_SUCCESS;
