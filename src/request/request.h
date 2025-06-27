efb5df80c0a718cd05645c1d20034358d58b9127
aggalan <aggalan@itba.edu.ar>
2025-06-27 16:30:41 -0300

    reqParser structure

---
+++ src/request/request.h
@@ -0,0 +1,8 @@
+//
+// Created by Agustin Galan on 27/06/2025.
+//
+
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#endif //REQUEST_H
---
+++ src/request/request_parser.h
@@ -0,0 +1,87 @@
+#ifndef REQUEST_PARSER_H
+#define REQUEST_PARSER_H
+#include "../buffer.h"
+#include <stdint.h>
+#include <netinet/in.h>
+
+#define METHOD_SIZE 255
+#define SOCKS_VERSION 0x05
+#define PORT_BYTE_LENGHT 2
+#define REQ_MAX_DN_LENGHT 0xFF
+
+typedef enum cmd {
+    CONNECT = 0x01,
+    BIND = 0x02,
+    UDP_ASSOCIATE = 0x03
+} ReqCmd;
+
+typedef enum atyp {
+    IPV4 = 0x01,
+    DOMAINNAME = 0x03,
+    IPV6 = 0x04
+} ReqAtyp;
+
+
+typedef union adress {
+    struct in_addr ipv4;
+    uint8_t domainname[REQ_MAX_DN_LENGHT + 1];
+    struct in6_addr ipv6;
+
+
+ //   uint8_t bytes[REQ_MAX_DN_LENGHT + 1];
+} ReqAddr;
+
+typedef enum request_state {
+    REQ_VERSION = 0,
+    REQ_CMD,
+    REQ_RSV,
+    REQ_ATYP,
+    REQ_DNLEN,
+    REQ_DST_ADDR,
+    REQ_DST_PORT,
+    REQ_ERROR,
+    REQ_END
+} ReqState;
+
+typedef enum request_status {
+    REQ_SUCCEDED = 0,
+    REQ_ERROR_GENERAL_FAILURE,
+    REQ_ERROR_CONNECTION_NOT_ALLOWED,
+    REQ_ERROR_NTW_UNREACHABLE,
+    REQ_ERROR_HOST_UNREACHABLE,
+    REQ_ERROR_CONNECTION_REFUSED,
+    REQ_ERROR_TTL_EXPIRED,
+    REQ_ERROR_COMMAND_NOT_SUPPORTED,
+    REQ_ERROR_ADDRESS_TYPE_NOT_SUPPORTED,
+} ReqStatus;
+
+
+typedef struct request_parser {
+    uint8_t version;
+    ReqCmd cmd;
+    ReqAtyp atyp;
+    uint8_t dnlen;
+    ReqAddr dst_addr;
+    in_port_t dst_port;
+    ReqState state;
+    ReqStatus status;
+} ReqParser;
+
+typedef enum request_codes {
+    REQ_OK = 0,
+    REQ_FULLBUFFER,
+} ReqCodes;
+
+//const char * reqParserToString(const ReqParser * p);
+
+void initRequestParser(ReqParser * p);
+
+ReqState requestParse(ReqParser* p, buffer* buffer);
+
+bool hasRequestReadEnded(ReqParser * p);
+
+bool hasRequestErrors(ReqParser * p);
+
+ReqCodes fillRequestAnswer( ReqParser * p, buffer* buffer);
+
+#endif //REQUEST_PARSER_H
