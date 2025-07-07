c3fda5410e0025e765dbf39e7d2f576a036e3081
ivovila <ivilamowski@itba.edu.ar>
2025-07-07 18:50:31 -0300

    Change in admin

--- src/admin/admin.h
+++ src/admin/admin.h
@@ -4,10 +4,7 @@
 #define USER_FILE "./src/authentication/users.txt"
 
 int admin_add_user(const char *username, const char *password);
-
 int admin_del_user(const char *username);
-
-
 char *admin_list_users(void);
 
 #endif // ADMIN_H
