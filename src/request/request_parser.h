c1ea020e38d624a0db687f5ac490a2d94f1bc7c5
aggalan <aggalan@itba.edu.ar>
2025-07-08 00:21:52 -0300

    fix

--- src/request/request_parser.h
+++ src/request/request_parser.h
@@ -85,6 +85,6 @@ bool has_request_read_ended(ReqParser * p);
 
 bool has_request_errors(ReqParser * p);
 
-ReqCodes fill_request_answer(ReqParser * p, buffer* buffer);
+ReqCodes fill_request_answer(ReqParser * p, buffer* buffer, struct selector_key * key);
 
 #endif //REQUEST_PARSER_H
